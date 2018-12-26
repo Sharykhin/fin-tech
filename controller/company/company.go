41ba9a464c4b6080aa3194e66f6ba7b94c090333
Sharykhin <siarhei.sharykhin@gmail.com>
2018-12-27 00:06:26 +0300

    some tricks

--- controller/company/company.go
+++ controller/company/company.go
@@ -3,16 +3,19 @@ package company
 import (
 	"github.com/Sharykhin/fin-tech/contract"
 	"github.com/Sharykhin/fin-tech/database"
+	"github.com/Sharykhin/fin-tech/service/logger"
 )
 
 type (
 	CompanyController struct {
 		storage contract.CompanyStorage
+		logger  contract.Logger
 	}
 )
 
 func NewCompanyController() *CompanyController {
 	return &CompanyController{
 		storage: database.NewCompanyStorage(),
+		logger:  logger.Logger,
 	}
 }
--- controller/company/update.go
+++ controller/company/update.go
@@ -11,6 +11,7 @@ import (
 func (cc CompanyController) Update(ctx context.Context, ID int64, ucr request.UpdateCompanyRequest) error {
 	err := cc.storage.Update(ctx, ID, ucr)
 	if err != nil {
+		cc.logger.Error("controller could not update a company on the storage: %v", err)
 		return err
 	}
 
